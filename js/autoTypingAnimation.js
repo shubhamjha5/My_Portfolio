00ed49181e0561a3ced9b30cbae9af409973c45c
Shubham Sourav <110170581+shubhamjha5@users.noreply.github.com>
2023-03-30 20:23:23 +0530

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Shubham Sourav</span> Currently I am pursuing my BTech in Electronics and Communication Engineering branch from BIT Sindri. I have a good knowledge in problem solving and have a very good command in Java programming language. I have also learnt Full Stack Web Development and designed man beautiful projects using it. I am willing to gain more and more knowledege and gain some experience in these feilds"
+      ],
+      typeSpeed: 6,
+      contentType: 'html'
+    });
+  }, 100);
